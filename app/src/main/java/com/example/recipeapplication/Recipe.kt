80c071f39e1ab7a3e2533601bbf5ea0ebe974e3d
Danielruwa <51158353+Danielruwa@users.noreply.github.com>
2023-08-05 10:39:49 +0300

    Update Recipe.kt

--- app/src/main/java/com/example/recipeapplication/Recipe.kt
+++ app/src/main/java/com/example/recipeapplication/Recipe.kt
@@ -1,8 +1,14 @@
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(tableName = "recipe")
 data class Recipe(
+    @PrimaryKey(autoGenerate = true)
     val title: String,
     val type: String,
     val serves: Int,
     val difficulty: String,
     val ingredients: List<String>,
     val preparationSteps: List<String>
-)
+):Serializable
